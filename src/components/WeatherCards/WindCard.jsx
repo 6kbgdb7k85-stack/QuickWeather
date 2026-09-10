13b1bea6ace1c617bb681e3e4136e3cec6822475
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-09-09 21:49:57 -0400

    WindCard complete

--- src/components/WeatherCards/WindCard.jsx
+++ src/components/WeatherCards/WindCard.jsx
@@ -1,5 +1,64 @@
-const WindCard = () => {
-  return <div>WindCard</div>;
-};
+import { Card, CardHeader, Skeleton, Stack } from "@mui/material";
+import React from "react";
+import {
+    WiDirectionDown,
+  WiDirectionDownLeft,
+  WiDirectionDownRight,
+  WiDirectionLeft,
+  WiDirectionRight,
+  WiDirectionUp,
+  WiDirectionUpLeft,
+  WiDirectionUpRight,
+} from "weather-icons-react";
 
-export default WindCard;
+const ICON_SIZE = 150;
+
+export default function WindCard({ speed, direction, unit, isLoading }) {
+  function windDirection(direction) {
+    const normalizedDirection = ((direction % 360) + 360) % 360;
+    const compassDirection = Math.round(normalizedDirection / 45) % 8;
+    switch (compassDirection) {
+      case 0:
+        return <WiDirectionUp size={ICON_SIZE} />;
+      case 1:
+        return <WiDirectionUpRight size={ICON_SIZE} />;
+      case 2:
+        return <WiDirectionRight size={ICON_SIZE} />;
+      case 3:
+        return <WiDirectionDownRight size={ICON_SIZE} />;
+      case 4:
+        return <WiDirectionDown size={ICON_SIZE} />;
+      case 5:
+        return <WiDirectionDownLeft size={ICON_SIZE} />;
+      case 6:
+        return <WiDirectionLeft size={ICON_SIZE} />;
+      case 7:
+        return <WiDirectionUpLeft size={ICON_SIZE} />;
+    }
+  }
+
+  if (isLoading) {
+    return (
+      <Card sx={{ height: "100%", textAlign: "center" }}>
+        <Stack spacing={1}>
+          <Skeleton variant="rectangular" width={"100%"} height={300} />
+          <Skeleton variant="rectangular" width={"100%"} height={100}/>
+        </Stack>
+      </Card>
+    );
+  }
+
+  if (!speed || !direction || !unit) {
+    return <></>;
+  }
+
+  return (
+    <Card sx={{ height: "100%", textAlign: "center" }}>
+    <CardHeader title={<h3>Wind Speed</h3>}/>
+      {windDirection(Number(direction))}
+      <h2>
+        {speed} {unit}
+      </h2>
+    </Card>
+  );
+}
