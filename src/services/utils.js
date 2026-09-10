9052f84876d53149dbc788647b979191e1e7b5bd
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-09-09 21:28:44 -0400

    created utilty method to set theme based on weather condition code

---
+++ src/services/utils.js
@@ -0,0 +1,46 @@
+import { clearTheme, cloudyTheme, rainyTheme } from "../assets/themes";
+
+export function getTheme(weatherCode) {
+  switch (Number(weatherCode)) {
+    case 0:
+    //partly cloudy code. close enough to clear to use the clearTheme
+    case 1:
+      return clearTheme;
+    //drizzle and freezing drizzle codes
+    case 51:
+    case 53:
+    case 55:
+    case 56:
+    case 57:
+    //rain and freezing rain codes
+    case 61:
+    case 63:
+    case 65:
+    case 66:
+    case 67:
+    //rain shower codes
+    case 80:
+    case 81:
+    case 82:
+    //thunderstorm codes
+    case 95:
+    case 96:
+    case 99:
+      return rainyTheme;
+    // cloudy codes
+    case 2:
+    case 3:
+    case 45:
+    case 48:
+    //snow codes. cloudy theme seemed good choice since an independent snow theme would be similar
+    case 71:
+    case 73:
+    case 75:
+    case 77:
+    case 85:
+    case 86:
+      return cloudyTheme;
+    default:
+      return clearTheme;
+  }
+}
